d3703ad9051ab1619305f0de35c8fae5d3641e71
home <ibasha36@gmail.com>
2022-07-26 18:27:39 +0530

    changes changes in it assets

--- asst/assetapp/views.py
+++ asst/assetapp/views.py
@@ -229,12 +229,12 @@ def new(request):
         form.save()
         if form.is_valid():
             student = form.save(commit=False)
-            # for z in HDDS:
-            #     if request.POST[z[0]] != '':
-            #         student.hdd = request.POST[z[0]]
-            # for z in OS_VERSIONS:
-            #     if request.POST[z[0]] != '':
-            #         student.Operating_System_Version = request.POST[z[0]]
+            for z in HDDS:
+                if request.POST[z[0]] != '':
+                    student.hdd = request.POST[z[0]]
+            for z in OS_VERSIONS:
+                if request.POST[z[0]] != '':
+                    student.Operating_System_Version = request.POST[z[0]]
             student.save()
             return index(request)
         else:
